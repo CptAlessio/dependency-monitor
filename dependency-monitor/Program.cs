6f7dc228266e8cc3dbd0e70065c647f95d99cb8d
alessio.marziali <alessio.marziali@truelayer.com>
2021-05-07 16:09:47 -0700

    + Added support for repos in ZIP format

--- dependency-monitor/Program.cs
+++ dependency-monitor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 
 namespace dependency_monitor
 {
@@ -10,9 +12,28 @@ namespace dependency_monitor
     {
         static void Main(string[] args)
         {
-            if (!CheckArguments(args)) return;
+            // ADD CODEOWNERS
             
-            GetDependencies(args[0], args[1]);
+            if (!CheckArguments(args)) return;
+
+            // args[0]
+            string zipFilePath = args[0];
+            // args[1]
+            string localPath = args[1];
+            string referenceName = args[2];
+
+            unzipRepository(zipFilePath, localPath);
+            List<string> projectFiles = returnAllFiles(localPath);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Found {0} C# Project files in archive", projectFiles.Count.ToString());
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+            
+            foreach (string csProjectFile in projectFiles)
+            {
+                GetDependencies(referenceName, csProjectFile);
+            }
         }
 
         /// <summary>
@@ -22,16 +43,16 @@ namespace dependency_monitor
         /// <returns></returns>
         private static bool CheckArguments(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 3)
             {
                 Console.WriteLine("ERROR: missing path");
                 return false;
             }
             else
             {
-                if (!args[1].EndsWith(".csproj"))
+                if (!args[0].EndsWith(".zip"))
                 {
-                    Console.WriteLine("Missing C# project file...");
+                    Console.WriteLine("Missing project-code ZIP file...");
                     return false;
                 }
             }
@@ -44,6 +65,7 @@ namespace dependency_monitor
         /// <returns></returns>
         private static void GetDependencies(string targetReferenceName, string path)
         {
+            int counter = 0;
             // Loop through CSPROJ file and look for Dependencies
             using (var file = new StreamReader(path))
             {
@@ -54,21 +76,76 @@ namespace dependency_monitor
                     {
                         line = line.Replace("<PackageReference Include=", "Dependency=");
                         line = line.Replace("/>", "");
+                        
                         if (line.ToLower().Contains(targetReferenceName.ToLower()))
                         {
                             // Highlight vulnerable dependency in Red
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine(line);
+                            counter++;
                         }
                         else
                         {
                             // Normal Values are Green
-                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.WriteLine(line);
                         }
                     }
                 }
             }
+
+            if (counter == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" - [SUCCESS] Target reference not found");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(" - [ALERT] Target reference found {0} times" , counter.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Used to unzip repository code
+        /// </summary>
+        /// <param name="zipFilePath"></param>
+        /// <param name="output"></param>
+        private static void unzipRepository(string zipFilePath, string output)
+        {
+            ZipFile.ExtractToDirectory(zipFilePath, output);
+        }
+
+        /// <summary>
+        /// Return all csproject files in the solution file
+        /// </summary>
+        /// <param name="sDir"></param>
+        /// <returns></returns>
+        private static List<String> returnAllFiles(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    if (f.EndsWith(".csproj"))
+                    {
+                        files.Add(f);
+                    }
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(returnAllFiles(d));
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            return files;
         }
         
     }
